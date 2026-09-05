95c5b940ac91dd7c4ca9d670634e325e07a3ea89
OOLlebedenko <oolebedenko@gmail.com>
2026-09-05 19:44:59 +0300

    refactor: add OSM feature source abstraction

--- topoprofile/osm/overpass/source.py
+++ topoprofile/osm/overpass/source.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 from topoprofile.geo.models import Bounds
 from topoprofile.osm.geojson import (
     GeoJSON,
@@ -7,7 +9,18 @@ from topoprofile.osm.overpass.client import OverpassClient
 from topoprofile.osm.overpass.queries.base import OverpassQuery
 
 
-class OverpassFeatureSource:
+class OSMFeatureSource(ABC):
+    """Base interface for loading OSM feature data."""
+
+    @abstractmethod
+    def load(
+            self,
+            bounds: Bounds,
+    ) -> GeoJSON:
+        """Load OSM features for the given geographic bounds."""
+
+
+class OverpassFeatureSource(OSMFeatureSource):
     """Load OSM features through the Overpass API."""
 
     def __init__(
