a547e0639064d46adccc4de217ede9c9f120f779
OOLlebedenko <oolebedenko@gmail.com>
2026-09-08 13:23:43 +0300

    fix: filter invalid road shield references in Liberty style

--- static/js/map.js
+++ static/js/map.js
@@ -9,31 +9,10 @@ import { setupMissingImageFallback } from "./base-map/images.js";
 import { transformBaseMapStyle } from "./base-map/style.js";
 import { MAP_CONFIG } from "./config.js";
 
-// Registers a transparent fallback for missing base-map icons.
-function setupMissingImageFallback(map) {
-    map.on("styleimagemissing", event => {
-        const imageId = event.id;
-
-        if (map.hasImage(imageId)) {
-            return;
-        }
-
-        const size = 16;
-        const data = new Uint8Array(size * size * 4);
-
-        map.addImage(imageId, {
-            width: size,
-            height: size,
-            data,
-        });
-    });
-}
-
 // Creates and returns the main application map.
 export function createMap(container) {
     const map = new MapLibreMap({
         container,
-        style: MAP_CONFIG.style,
         center: MAP_CONFIG.center,
         zoom: MAP_CONFIG.zoom,
         minZoom: MAP_CONFIG.minZoom,
@@ -42,6 +21,13 @@ export function createMap(container) {
         minPitch: MAP_CONFIG.minPitch,
         maxPitch: MAP_CONFIG.maxPitch,
     });
+
+    map.setStyle(
+        MAP_CONFIG.style,
+        {
+            transformStyle: transformBaseMapStyle,
+        },
+    );
 
     setupMissingImageFallback(map);
 
